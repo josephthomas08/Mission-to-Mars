fb3a719982d0feebe3d7fd2f6c595f534eb32ab8
josephthomas08 <joseph@tmgcontrol.com>
2021-03-03 16:06:43 -0500

    Mission to mars scraping websites

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
-
-
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -13,16 +11,13 @@ mongo = PyMongo(app)
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
-
-
    @app.route("/scrape")
-def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return redirect('/', code=302)
-
-   .update(query_parameter, data, options)
+   
+   def scrape():
+       mars = mongo.db.mars
+       mars_data = scraping.scrape_all()
+       mars.update({}, mars_data, upsert=True)
+       return redirect('/', code=302)
 
    if __name__ == "__main__":
-   app.run()
+       app.run()
