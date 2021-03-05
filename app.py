d3c0a27206ff5bf06d0a98be659b0064262a01f0
josephthomas08 <joseph@tmgcontrol.com>
2021-03-04 19:51:49 -0500

    Mission to mars enhanced view of hemisphers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 app = Flask(__name__)
@@ -11,13 +11,13 @@ mongo = PyMongo(app)
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
-   @app.route("/scrape")
-   
-   def scrape():
-       mars = mongo.db.mars
-       mars_data = scraping.scrape_all()
-       mars.update({}, mars_data, upsert=True)
-       return redirect('/', code=302)
 
-   if __name__ == "__main__":
-       app.run()
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run()
